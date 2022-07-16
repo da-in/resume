da41e3767244b594ce76f861e0e98c13ef9d151b
da-in <talown@naver.com>
2022-07-16 18:14:31 +0900

    :rocket: CI Test
    CI Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import birdhead from './assets/birdhead.jpg';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>This is CI Test Text</p>
+                <img
+                    src={birdhead}
+                    style={{ width: 300, height: 'auto' }}
+                    className="App-logo"
+                    alt="birdhead"
+                />
+                {/* <img src={birdhead} className="App-logo" alt="logo" /> */}
+                <br />
+                <br />
+                <p>최다인 포폴</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
