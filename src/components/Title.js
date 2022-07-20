c6689a4a4c09342db7452fc29b534ceb8de6ce49
da-in <talown@naver.com>
2022-07-20 20:31:04 +0900

    :sparkles: Typing Function
    Typing Function

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,26 +5,29 @@ const TitleDiv = styled.div`
     font-size: ${({ theme }) => theme.fontSizes.titleSize};
 `;
 
-const TEXT = ['최다인', 'Choi Da-in', '崔多仁', 'Айсулу'];
+const TEXT = ['가나다라마바사아자차카타파하', 'Choi Da-in', '崔多仁', 'Айсулу'];
 
 function Title() {
-    const [title, setTitle] = useState(0);
+    const [title, setTitle] = useState('');
+    const [count, setCount] = useState(0);
     const index = useRef(0);
-    useEffect(() => {
-        const loop = setInterval(() => {
-            index.current += 1;
-            setTitle(index.current);
-            if (index.current === 10) {
-                clearInterval(loop);
-            }
-        }, 1500);
-    }, []);
 
-    return (
-        <>
-            <TitleDiv>{TEXT[title % 4]}</TitleDiv>
-        </>
-    );
+    useEffect(() => {
+        let target = TEXT[index.current];
+        const loop = setInterval(() => {
+            setTitle((Text) => {
+                let updated = Text;
+                updated = Text + target[count];
+                return updated;
+            });
+            setCount(count + 1);
+        }, 150);
+        count === target.length && clearInterval(loop);
+        return () => {
+            clearInterval(loop);
+        };
+    });
+    return <TitleDiv>{title}</TitleDiv>;
 }
 
 export default Title;
