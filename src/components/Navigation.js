d2917c0627ef9b45b20ca18a29ac92f9b213423f
da-in <talown@naver.com>
2022-10-02 00:25:06 +0900

    :sparkles: Navigation
    Navigation #3

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import sign from '../assets/choidain.svg';
+
+const NavTitle = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 20px;
+`;
 
 function Navigation() {
     return (
         <div
-            style={{ display: 'flex', flexDirection: 'column' }}
-            className="handwriting"
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                // minWidth 추후 제거
+                minWidth: '180px',
+            }}
         >
-            <div>최다인 포트폴리오</div>
-            <div>Contact</div>
-            <div>Skill</div>
-            <div>Project</div>
-            <div>Timeline</div>
+            <NavTitle>
+                <img src={sign} alt="sign" width="70px" />
+                <div>포트폴리오</div>
+            </NavTitle>
+            <div className="handwriting">Contact</div>
+            <div className="handwriting">Skill</div>
+            <div className="handwriting">Project</div>
+            <div className="handwriting">Timeline</div>
         </div>
     );
 }
