f80cd45b8c9fabbe7b9f4f9a86bbfa6fb9667540
da-in <talown@naver.com>
2022-10-01 23:52:24 +0900

    :lipstick: Handwriting Font
    Handwriting Font #3

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Navigation() {
     return (
-        <div style={{ display: 'flex', backgroundColor: 'red' }}>
-            navigation
+        <div
+            style={{ display: 'flex', flexDirection: 'column' }}
+            className="handwriting"
+        >
+            <div>최다인 포트폴리오</div>
+            <div>Contact</div>
+            <div>Skill</div>
+            <div>Project</div>
+            <div>Timeline</div>
         </div>
     );
 }
