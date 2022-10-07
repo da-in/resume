c4e653dd37e8cad8ad552a5197ed9377cd8ccff8
da-in <talown@naver.com>
2022-10-07 16:16:02 +0900

    :sparkles: Intro Section
    Intro Section #3

--- src/section/Intro.js
+++ src/section/Intro.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import ProfileImg from '../assets/profile.png';
+import styles from '../assets/styles';
 
 function Intro() {
     return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div>사진</div>
-            <div>
-                <p>안녕하세요. 프론트엔드 개발자 최다인 입니다.</p>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}
+        >
+            <img
+                src={ProfileImg}
+                alt="profile"
+                style={{
+                    width: '260px',
+                    height: '260px',
+                    objectFit: 'contain',
+                }}
+            />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                }}
+            >
+                <p style={styles.fontWeight.bold}>
+                    안녕하세요. 프론트엔드 개발자 최다인 입니다.
+                </p>
                 <p>
                     사람들과 함께 소통하고 협업하는 즐거움으로 개발에 임하고
                     있습니다. 뭐든지 직접 구현해보는 것을 좋아해서 Blog와
