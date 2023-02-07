5207f125e207ac0474d9b29a0ff62b315e1f3db0
da-in <talown@naver.com>
2023-02-07 18:09:14 +0900

    :lipstick: Update Section Title UI

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import theme from '../theme';
+import styled from 'styled-components';
+
+const Title = styled.div`
+  padding: 20px 0px 20px 0px;
+  margin-bottom: 20px;
+  border-bottom: solid 2px;
+  font-size: ${theme.font.big};
+  font-weight: 800;
+`;
 
 function SectionTitle({ children }) {
-    return (
-        <p
-            className="handwriting"
-            style={{
-                textAlign: 'center',
-                paddingTop: '30px',
-                paddingBottom: '15px',
-                fontSize: '32px',
-            }}
-        >
-            {children}
-        </p>
-    );
+  return <Title>{children}</Title>;
 }
 
 export default SectionTitle;
